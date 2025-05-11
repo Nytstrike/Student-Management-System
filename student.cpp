8a37462625ab384224cd0093cffdf835f815e1d3
Syed Muhammad Ali <ali.muhammad.naqvi.99@gmail.com>
2025-05-11 10:53:55 +0500

    Update student.cpp

--- student.cpp
+++ student.cpp
@@ -3,42 +3,46 @@
 #include<string>
 #include<sstream>
 #include<fstream>
-void Student::displayFromFile(const std::string &rollNumberToFind) {
-    std::ifstream in("studentdata.txt");
-    std::string line;
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+void Student::displayFromFile(const string &rollNumberToFind) {
+    std::ifstream in("student.txt");
+    string line;
     bool found = false;
 
     if (!in.is_open()) {
-        std::cerr << "Unable to open studentdata.txt\n";
+        std::cerr << "Unable to open student.txt\n";
         return;
     }
 
     while (getline(in, line)) {
         std::istringstream iss(line);
-        std::string fields[11];
+        string fields[11];
         for (int i = 0; i < 11; ++i) {
             if (!getline(iss, fields[i], ',')) break;
         }
 
         if (fields[0] == rollNumberToFind) {
             found = true;
-            std::cout << "\n--- Student Record ---\n";
-            std::cout << "Roll Number: " << fields[0] << "\n";
-            std::cout << "User ID: " << fields[1] << "\n";
-            std::cout << "Name: " << fields[2] << " " << fields[3] << "\n";
-            std::cout << "Email: " << fields[4] << "\n";
-            std::cout << "Phone: " << fields[5] << "\n";
-            std::cout << "Address: " << fields[6] << "\n";
-            std::cout<<"CNIC Number: " << fields[7] <<"\n";
-            std::cout<< "Degree: " << fields[8] << "\n";
-            std::cout<< "CPGA: " <<fields[9] <<"\n";
+            cout << "\n--- Student Record ---\n";
+            cout << "Roll Number: " << fields[0] << "\n";
+            cout << "User ID: " << fields[1] << "\n";
+            cout << "Name: " << fields[2] << " " << fields[3] << "\n";
+            cout << "Email: " << fields[4] << "\n";
+            cout << "Phone: " << fields[5] << "\n";
+            cout << "Address: " << fields[6] << "\n";
+            cout<<"CNIC Number: " << fields[7] <<"\n";
+            cout<< "Degree: " << fields[8] << "\n";
+            cout<< "CPGA: " <<fields[9] <<"\n";
         }
     }
 
     in.close();
 
     if (!found) {
-        std::cout << "No student found with roll number: " << rollNumberToFind << "\n";
+        cout << "No student found with roll number: " << rollNumberToFind << "\n";
     }
 }
 
