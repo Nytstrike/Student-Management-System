a3c717255ec0fe1ce9e483187a2b46344bf0eb47
Syed Muhammad Ali <ali.muhammad.naqvi.99@gmail.com>
2025-05-11 21:44:17 +0500

    Add files via upload

---
+++ include/functions.hpp
@@ -0,0 +1,9 @@
+#pragma once
+#include<iostream>
+using std::string;
+void loadingBar();
+void ensureDataFileExists();
+void clearInputBuffer();
+void printCentered(const std::string& text);
+int totalUserCount();
+string getPasscode();
