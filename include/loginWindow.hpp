cdda9956fdb7983be3f728fda8485ceb2bc26c02
NeerajKhemani <neerajkhemani009@gmail.com>
2025-05-12 02:40:44 +0500

    Update loginWindow.hpp

--- include/loginWindow.hpp
+++ include/loginWindow.hpp
@@ -2,9 +2,9 @@
 #include<iostream>
 using std::string;
 
-void printCentered(const std::string &text);
+void printCentered(const string &text);
 void moveCursorTo(int x, int y);
 void getTerminalSize(int& width, int& height);
-void printAtBottom(const std::string& text);
+void printAtBottom(const string& text);
 void preloginWindow();
 void loginOptions();
